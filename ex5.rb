ee0ef9851ff20c765961d8ccdd7f2d9e1e055935
ux9 <ux9@users.noreply.github.com>
2015-06-28 21:27:08 -0700

    ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,12 @@
+full_name = 'eric cartman'
+age = 8 
+occupation = 'student'
+gender = 'male'
+quotes = 'SCREW U GUYS I M GOING HOME'
+city = 'south park '
+
+puts "what s up guys! I m #{full_name}  i m #{occupation} from  #{city} "
+puts 'wait...'
+puts '...'
+puts quotes
+
